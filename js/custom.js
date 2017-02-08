db2af574ed40b48df9d8618cfe11ae822c8caecf
Shreel Desai <shreel@promactinfo.com>
2017-02-08 15:19:03 +0530

    Added Favicon & other meta tags. Renamed some class names.

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,30 @@
 //Activate Wow.js
 new WOW().init();
 
-//Call to Parallax
-$(document).ready(function (e) {
-    if ($('.parallax-window').length) {
-        $('.parallax-window').parallax();
-    }
-});
-
 //Slide to particular section
 var timer = 1000;
 var offsetVal = 20;
-$(".amaze-home-link").click(function () {
+$(".home-link").click(function () {
     $('html, body').animate({
-        scrollTop: $(".amaze-home").offset().top - offsetVal
+        scrollTop: $(".section-home").offset().top - offsetVal
     }, timer);
     return false;
 });
-$(".amaze-about-link").click(function () {
+$(".about-link").click(function () {
     $('html, body').animate({
-        scrollTop: $(".amaze-about").offset().top - offsetVal
+        scrollTop: $(".section-about").offset().top - offsetVal
     }, timer);
     return false;
 });
-$(".amaze-services-link").click(function () {
+$(".services-link").click(function () {
     $('html, body').animate({
-        scrollTop: $(".amaze-services").offset().top - offsetVal
+        scrollTop: $(".section-services").offset().top - offsetVal
     }, timer);
     return false;
 });
-$(".amaze-portfolio-link").click(function () {
+$(".portfolio-link").click(function () {
     $('html, body').animate({
-        scrollTop: $(".amaze-portfolio").offset().top - offsetVal
+        scrollTop: $(".section-portfolio").offset().top - offsetVal
     }, timer);
     return false;
 });
