6dffc8bb69b30f29c454759333973b013d832b82
Shreel Desai <shreel@promactinfo.com>
2017-02-08 18:00:05 +0530

    Implemented gulp.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      
+    },
+  })
+});
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('scss/**/*.scss', ['sass']); 
+  gulp.watch('**/*.html', browserSync.reload); 
+  gulp.watch('js/**/*.js', browserSync.reload);
+});
