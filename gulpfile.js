57d84bb283542d8fcf8e0479692841e38cf52d09
Shreel Desai <shreel@promactinfo.com>
2017-02-09 10:38:26 +0530

    Added default task & renamed 1 task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var serve = require('browser-sync').create();
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      
-    },
+gulp.task('serve', function() {
+  serve.init({
+    server: { },
   })
 });
 gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss
+  return gulp.src('scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
+    .pipe(serve.reload({
       stream: true
     }))
 });
-gulp.task('watch', ['browserSync', 'sass'], function (){
+gulp.task('watch', ['serve', 'sass'], function (){
   gulp.watch('scss/**/*.scss', ['sass']); 
-  gulp.watch('**/*.html', browserSync.reload); 
-  gulp.watch('js/**/*.js', browserSync.reload);
+  gulp.watch('**/*.html', serve.reload); 
+  gulp.watch('js/**/*.js', serve.reload);
 });
+// default task
+gulp.task('default', ['watch']);
